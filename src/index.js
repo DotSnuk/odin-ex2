dcfe9a86f884390d90853ca357f984e5bfa17e36
Hampus Farner <hampusfarner@outlook.com>
2024-03-07 13:20:02 +0100

    my solution (fibRec) with some added community solution (cfibsRec) mostly to help me understand their logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,59 @@
-let company = {
-  sales: [
-    { name: 'hampus', salary: 30000 },
-    { name: 'marica', salary: 20000 },
-  ],
-  development: {
-    sites: [
-      { name: 'Peter', salary: 25000 },
-      { name: 'Alex', salary: 18000 },
-    ],
-    internals: [{ name: 'Jack', salary: 13000 }],
-  },
-};
+// let company = {
+//   sales: [
+//     { name: 'hampus', salary: 30000 },
+//     { name: 'marica', salary: 15000 },
+//   ],
+//   development: {
+//     sites: [
+//       { name: 'Peter', salary: 25000 },
+//       { name: 'Alex', salary: 18000 },
+//     ],
+//     internals: [{ name: 'Jack', salary: 13000 }],
+//   },
+// };
 
-function sumSalaries(department) {
-  if (Array.isArray(department)) {
-    return department.reduce((prev, current) => prev + current.salary, 0);
-  }
+// function sumSalaries(department) {
+//   if (Array.isArray(department)) {
+//     return department.reduce((prev, current) => prev + current.salary, 0);
+//   }
+//   let sum = 0;
+//   for (let subdep of Object.values(department)) {
+//     sum += sumSalaries(subdep);
+//   }
+//   return sum;
+// }
+
+// console.log(sumSalaries(company));
+
+function fibSeq(n, sum = 0) {
+  if (curN !== n) fibSeq(curN + 1);
 }
+
+function fibIteration(n) {
+  let i = [];
+  while (n > i.length) {
+    if (i.length < 1) i.push(0);
+    if (i.length < 2) i.push(1);
+    i.push(i.slice(-2).reduce((tot, cur) => tot + cur, 0));
+  }
+  console.log(i);
+}
+// fibIteration(8);
+
+function fibRec(n, list = [0, 1]) {
+  list.push(list.slice(-2).reduce((tot, cur) => tot + cur, 0));
+  if (list.length < n) fibRec(n, list);
+  return list;
+}
+// console.log(fibRec(10));
+
+function cfibsRec(n) {
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  return [...cfibsRec(n - 1), cfibsRec(n - 1)[n - 2] + cfibsRec(n - 2)[n - 3]];
+}
+console.log(cfibsRec(5));
+
+// fib(6);
+// let i = [1, 2, 3, 4, 5];
+// console.log(i.slice(-2).reduce((tot, cur) => tot + cur, 0));
