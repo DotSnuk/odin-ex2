81e70d645bc750d534e49a2eb9a5aa28670ea730
Hampus Farner <hampusfarner@outlook.com>
2024-03-08 14:24:44 +0100

    mergesorted sorted :D

--- src/index.js
+++ src/index.js
@@ -52,7 +52,62 @@ function cfibsRec(n) {
   if (n === 2) return [0, 1];
   return [...cfibsRec(n - 1), cfibsRec(n - 1)[n - 2] + cfibsRec(n - 2)[n - 3]];
 }
-console.log(cfibsRec(5));
+// console.log(cfibsRec(5));
+
+const arrayUnsorted = [3, 2, 1, 13, 8, 5, 0, 1];
+const arraySorted = [];
+let arrayMerge = [];
+
+function sort(array) {
+  if (array.length === 1) return array;
+  if (array[0] > array[1]) return array[1];
+  return array[0];
+}
+
+function merge(left, right) {
+  const merged = [];
+  let lIndx = 0;
+  let rIndx = 0;
+  while (lIndx < left.length && rIndx < right.length) {
+    if (left[lIndx] < right[rIndx]) {
+      merged.push(left[lIndx]);
+      lIndx += 1;
+    } else {
+      merged.push(right[rIndx]);
+      rIndx += 1;
+    }
+  }
+  while (lIndx < left.length) {
+    merged.push(left[lIndx]);
+    lIndx += 1;
+  }
+  while (rIndx < right.length) {
+    merged.push(right[rIndx]);
+    rIndx += 1;
+  }
+
+  console.log(`merged ${merged}`);
+  return merged;
+}
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  const [...arrHalfLeft] = array.slice(0, array.length / 2);
+
+  const [...arrHalfRight] = array.slice(array.length / 2);
+
+  return merge(mergeSort(arrHalfLeft), mergeSort(arrHalfRight));
+  // if (arrayMerge.length > 1) merge(arrayMerge[0], arrayMerge[1]);
+  // console.log(`left ${arrHalfLeft}`);
+  // console.log(`right ${arrHalfRight}`);
+  // mergeSort(arrHalfLeft);
+  // const [...arrHalfRight] = array.slice(array.length / 2);
+}
+console.log(mergeSort(arrayUnsorted));
+
+// console.log(mergeSort(arrayUnsorted));
 
 // fib(6);
 // let i = [1, 2, 3, 4, 5];
